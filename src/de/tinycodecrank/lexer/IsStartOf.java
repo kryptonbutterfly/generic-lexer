a81d27d7653d26839756bedafb765baf4b574608
tinycodecrank <tinycodecrank@gmail.com>
2023-08-07 18:20:33 +0200

    add missing @FunctionalInterface

--- src/de/tinycodecrank/lexer/IsStartOf.java
+++ src/de/tinycodecrank/lexer/IsStartOf.java
@@ -5,6 +5,7 @@ import de.tinycodecrank.functions.applicable.ApplicableLeft;
 import de.tinycodecrank.functions.int_.IntBiPredicate;
 import de.tinycodecrank.functions.int_.applicable.IntApplicableRight;
 
+@FunctionalInterface
 public interface IsStartOf extends ApplicableLeft<String, IntBiPredicate>, IntApplicableRight<ObjIntBiPredicate<String>>
 {
 	boolean test(String target, int offset, int start);
